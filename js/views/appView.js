5757c1e3162518b26ba1a4f251c6c4bb76717c1f
mathildathompson <mathildathompson288@gmail.com>
2014-09-01 21:48:06 +0100

    Before dangerous adding click events to categories

--- js/views/appView.js
+++ js/views/appView.js
@@ -3,13 +3,17 @@ var MonthBark = MonthBark || {};
 MonthBark.AppView = Backbone.View.extend({
   el: '#main',
   events: {
-    'click .sidebar-link': 'favourites',
+    'click .sidebar-link': 'render',
     'click .brands': 'brands'
   },
   initialize: function () {
     this.template = _.template($('#appView').html());
   },
-  render: function () {
+  render: function (event) {
+    if(event){
+      var category = event.currentTarget.attributes.id
+      debugger;
+    }
     this.$el.html(this.template());
     $.each(this.collection, function(index, pick){
       //Loop through the array of products;
